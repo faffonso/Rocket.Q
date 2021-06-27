7e6c65e70bbfb27378643f4df886f67d1c7a8a4e
faffonso <francisco.affonso02@gmail.com>
2021-06-26 23:34:11 -0300

    Day 5 - NLW#6

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,5 +1,7 @@
 module.exports = {
-    index (req, res){
+    async index (req, res){
+        const db = await Database()
+
         const roomId = req.params.room
         const questionId = req.params.question
         const action = req.params.action
@@ -7,5 +9,39 @@ module.exports = {
 
         console.log(`room = ${roomId}, questionId = ${questionId}
         action = ${action}, password = ${password}`)
+
+        const verifyRoom = await db.get(`SELECT * FROM rooms WHERE id = ${roomId}`)
+        if(verifyRoom.pass == password){
+            if(action == "delete"){
+                await db.run(`DELETE FROM questions WHERE id = ${questionId}`)
+            } else if(action == "check"){
+                await db.run(`UPDATE questions SET read = 1 WHERE id = ${questionId}`)
+            }
+            res.redirect(`/room/${roomId}`)
+        } 
+        
+        else{
+            res.render('passincorrect', {roomId: roomId})
+        }
+
+
+    },
+
+    async create(req, res){
+        const db = await Database()
+        const question = req.body.question
+        const roomId = req.params.room
+
+        await db.run(`INSERT INTO questions(
+            title,
+            room,
+            read
+        )VALUES(
+            "${question}",
+            ${roomId},
+            0
+        )`)
+
+        res.redirect(`/room/${roomId}`)
     }
 }
